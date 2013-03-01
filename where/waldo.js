bf4e246050e99ed8ddb2a3eb3048f1e24ac32a39
Jason Schneiderman <jason.schneiderman@tufts.edu>
2013-03-01 12:00:22 -0500

    info window

--- where/waldo.js
+++ where/waldo.js
@@ -2,30 +2,37 @@ var image = "mbta_red_100.png";
 function initialize() {
 	mapOptions = {
           center: new google.maps.LatLng(42.360996,-71.075478),
-          zoom: 12,
+          zoom: 11,
           mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
     get_location();
 }
 function get_location() {
-	console.log("test1");
 	if (navigator.geolocation) {
-		console.log("test2");
-    	navigator.geolocation.getCurrentPosition(make_marker);
+    	navigator.geolocation.getCurrentPosition(make_marker, make_window);
+    	
 	}
 }
 
 
 function make_marker(position) {
-		console.log("test3");
 	lat = position.coords.latitude;
 	lng = position.coords.longitude;
 	var mylatlng = new google.maps.LatLng(lat,lng);
-	var location = new google.maps.Marker({
+	location = new google.maps.Marker({
 		position: mylatlng,
 		map: map,
 		title: "Current Location",
 		icon: image
 	});
 }
+
+function make_window(position) {
+	lat = position.coords.latitude;
+	lng = position.coords.longitude;
+	var mylatlng = new google.maps.LatLng(lat,lng);
+	var infowindow = new google.maps.InfoWindow({
+		content: "I am here at " + lat + ", " + lng
+	}); 
+	infowindow.open(map, location);
