2132cb52d63311d97ced9fef847ac807fb2b7af9
Jason Schneiderman <jason.schneiderman@tufts.edu>
2013-03-02 16:54:15 -0500

    fixed toRad function

--- where/waldo.js
+++ where/waldo.js
@@ -73,16 +73,19 @@ function closest(lat1, lng1) {
 }
 function distance(lat1, lat2, lng1, lng2) {
 	var R = 3963.1676; // miles
-	var dLat = (lat2-lat1).toRad();
-	var dLon = (lon2-lon1).toRad();
-	var lat1 = lat1.toRad();
-	var lat2 = lat2.toRad();
+	var dLat = toRad(lat2-lat1);
+	var dLon = toRad(lng2-lng1);
+	var lat1 = toRad(lat1);
+	var lat2 = toRad(lat2);
 
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	var d = R * c;
 	return d;
+}
+function toRad(x) {
+	return x * MATH.PI / 180;
 }
 function init_stops() {
 	for(i=0;i<t_coords.length;i++) {
