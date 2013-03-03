97212e71e195b8ec775bb7487328195ff5936d1d
Jason Schneiderman <jason.schneiderman@tufts.edu>
2013-03-03 17:30:13 -0500

    should work for carmen

--- where/waldo.js
+++ where/waldo.js
@@ -225,7 +225,9 @@ function handle_waldo_carmen() {
 		google.maps.event.addListener(carm_mark, 'click', function() {
   			carm_window.open(map,carm_mark);
 		});	
-	catch(error) {console.log("hi")}
+	catch(error) {
+		console.log(error.Message)
+	}
 	
 }
 
